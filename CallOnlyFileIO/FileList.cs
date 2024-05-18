5e85ce9354c73b1c90ddd8d8cd293464546b5e65
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-18 14:10:54 +0200

    Add wrapper around File.*

---
+++ CallOnlyFileIO/FileList.cs
@@ -0,0 +1,23 @@
+namespace SunamoFileIO.CopyFromFileIO;
+public class FileList
+{
+    public static async Task<List<string>> ReadAllLinesListAsync(string path)
+    {
+        return (await File.ReadAllLinesAsync(path)).ToList();
+    }
+
+    public static async Task<List<byte>> ReadAllBytesListAsync(string path)
+    {
+        return (await File.ReadAllBytesAsync(path)).ToList();
+    }
+
+    public static async Task WriteAllLinesListAsync(string path, IList<string> lines)
+    {
+        await File.WriteAllLinesAsync(path, lines.ToUnixLineEnding());
+    }
+
+    public static async Task WriteAllBytesListAsync(string path, List<byte> bytes)
+    {
+        await File.WriteAllBytesAsync(path, bytes.ToArray());
+    }
+}
---
+++ CallOnlyFileIO/FileText.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunamoFileIO.CallOnlyFileIO;
+internal class FileText
+{
+    public async Task<string> ReadAllText(string path)
+    {
+        return await File.ReadAllTextAsync(path);
+    }
+
+    public async Task WriteAllText(string path, string c)
+    {
+        await File.WriteAllTextAsync(path, c);
+    }
+}
