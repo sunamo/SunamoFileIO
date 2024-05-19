3f34bf80b51fe3c9cf0447654e2b84e9d17cb6c6
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-19 16:27:07 +0200

    Buildable

---
+++ _sunamo/StringExtensions.cs
@@ -0,0 +1,9 @@
+namespace SunamoFileIO;
+
+public static class StringExtensions
+{
+    public static string ToUnixLineEnding(this string s)
+    {
+        return s.ReplaceLineEndings(Consts.nl);
+    }
+}
