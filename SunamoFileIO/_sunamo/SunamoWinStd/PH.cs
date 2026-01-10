c64aea4945c77fe742a04fe648964faa8b701068
Radek Jancik <radek.jancik@usys.cz>
2026-01-10 10:26:01 +0100

    feat: removed empty classes

--- SunamoFileIO/_sunamo/SunamoWinStd/PH.cs
+++
@@ -1,9 +0,0 @@
-namespace SunamoFileIO._sunamo.SunamoWinStd;
-
-/// <summary>
-/// EN: Platform helper class (placeholder for Windows-specific functionality)
-/// CZ: Pomocná třída pro platformu (zástupce pro Windows-specifickou funkcionalitu)
-/// </summary>
-internal class PH
-{
-}
